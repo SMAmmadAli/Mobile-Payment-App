21b81dabb5b9cc328a62f8ed5d95c430a5c093ad
S M Ammad Ali <65030609+SMAmmadAli@users.noreply.github.com>
2023-01-03 11:23:38 -0800

    Day 1

--- lib/page/splash_screen.dart
+++ lib/page/splash_screen.dart
@@ -1,7 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:mobile_payment_app/page/loginpage.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LoginPage(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
